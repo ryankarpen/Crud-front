d86f855c015a03e408fb2ee965c4c8442cbff278
Ryan Karpen <ryankarpen2015@gmail.com>
2024-01-21 15:44:41 -0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
-import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteCommonjs()
-  ],
+  plugins: [react()],
 })
