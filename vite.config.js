9c5a7914d992fa8e20153d6a731eda20e36ef4a8
Ryan Karpen <ryankarpen2015@gmail.com>
2024-01-21 16:03:12 -0300

    axios config to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
+import rollupNodeResolve from 'rollup-plugin-node-resolve'
+import rolllupJson from 'rollup-plugin-json'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    viteCommonjs()
+    react(),
+    rollupNodeResolve({ jsnext: true, preferBuiltins: true, browser: true }),
+    rolllupJson(),
   ],
 })
