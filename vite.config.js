1fd737a917a6b0aab672bf016286ad3d5a7ab1bc
Ryan Karpen <ryankarpen2015@gmail.com>
2024-01-21 21:59:31 -0300

    vite last config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default {
   plugins: [
     react(),
     viteCommonjs()
   ],
-})
+}
